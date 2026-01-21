6eb33376b5c8ea6fe1990544883125aa447eb881
RichardPadilla07 <richard.padilla@epn.edu.ec>
2026-01-21 17:09:25 -0500

    veamossi ya pasamos a diseño

--- lib/core/utils/image_helper.dart
+++ lib/core/utils/image_helper.dart
@@ -4,11 +4,21 @@ import 'package:image_picker/image_picker.dart';
 class ImageHelper {
   static Future<XFile?> pickImageFromGallery() async {
     final picker = ImagePicker();
-    return await picker.pickImage(source: ImageSource.gallery);
+    // Solicitar permiso de galería
+    final permission = await picker.requestPermission();
+    if (permission != null && permission) {
+      return await picker.pickImage(source: ImageSource.gallery);
+    }
+    return null;
   }
 
   static Future<XFile?> pickImageFromCamera() async {
     final picker = ImagePicker();
-    return await picker.pickImage(source: ImageSource.camera);
+    // Solicitar permiso de cámara
+    final permission = await picker.requestPermission();
+    if (permission != null && permission) {
+      return await picker.pickImage(source: ImageSource.camera);
+    }
+    return null;
   }
 }
