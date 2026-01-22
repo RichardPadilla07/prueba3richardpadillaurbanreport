c62c62f4e0626a60ae4e6b56d3e381d49ceefa10
RichardPadilla07 <richard.padilla@epn.edu.ec>
2026-01-22 13:39:10 -0500

    permisos valen

--- lib/core/utils/image_helper.dart
+++ lib/core/utils/image_helper.dart
@@ -1,10 +1,21 @@
 // Helper for image picking
+import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class ImageHelper {
   static Future<XFile?> pickImageFromGallery() async {
-    final status = await Permission.photos.request();
+    PermissionStatus status;
+    if (Platform.isAndroid) {
+      status = await Permission.storage.request();
+      // On Android 13+, permission_handler maps storage to READ_MEDIA_IMAGES when available
+      if (!status.isGranted) {
+        status = await Permission.photos.request();
+      }
+    } else {
+      status = await Permission.photos.request();
+    }
+
     if (status.isGranted) {
       final picker = ImagePicker();
       return await picker.pickImage(source: ImageSource.gallery);
